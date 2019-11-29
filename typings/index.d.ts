3c111a7f786d09bdfdfb70d7205a15e9f98929d5
itargaryen <gcy77@live.com>
2019-11-29 15:58:20 +0800

    update typing

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,16 +1,6 @@
-import Vue from 'vue';
-
-export interface InstallationOptions {
-  locale: any,
-  i18n: any,
-  size: string
-}
-
 declare namespace JsonViewer {
 }
 
 declare module "JsonViewer" {
   export = JsonViewer;
 }
-
-export function install (vue: typeof Vue, options: InstallationOptions): void
